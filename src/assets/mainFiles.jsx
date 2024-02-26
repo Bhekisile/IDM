8d8879efe0ed7015348cfd5383d2abfa4ea6f6aa
Bhekisile <bhekisilejozi@gmail.com>
2024-02-26 18:53:09 +0200

    Create table for the main body and add React icons and texts

---
+++ src/assets/mainFiles.jsx
@@ -0,0 +1,42 @@
+import { CiMusicNote1 } from "react-icons/ci";
+import { HiOutlineArrowDownOnSquareStack, HiOutlineStop } from "react-icons/hi2";
+import { IoDocumentTextOutline } from "react-icons/io5";
+import { LiaVideoSolid } from "react-icons/lia";
+import { MdOutlineBrokenImage } from "react-icons/md";
+
+const mainFiles = [
+    {
+      name: <MdOutlineBrokenImage />  + "UIUXMonster.png",
+      size: '745KB',
+    },
+    {
+      name: `${<CiMusicNote1 />} 2pacCover.mp3`,
+      size: '300MB',
+    },
+    {
+      name: `${<HiOutlineArrowDownOnSquareStack />} UIUXMonster.Zip`,
+      size: '1200MB',
+    },
+    {
+      name: `${<IoDocumentTextOutline />} Document.pdf`,
+      size: '2MB',
+    },
+    {
+      name: `${<LiaVideoSolid />} BetterCall Saul so2E10.720p.mp3`,
+      size: '25GB',
+    },
+    {
+      name: 'Call Of Duty.apk',
+      size: '1200MB',
+    },
+    {
+      name: `${<HiOutlineArrowDownOnSquareStack />} 2pacCover.mp3`,
+      size: '1200MB',
+    },
+    {
+      name: `${<HiOutlineStop />} Mima.exe`,
+      size: '32MB',
+    },
+  ];
+
+  export default mainFiles;
