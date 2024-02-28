fa0bd72fd4b4ecb623447e992414bba66b98eef8
Bhekisile <bhekisilejozi@gmail.com>
2024-02-28 07:28:28 +0200

    Add handleClick, handleSelectAll functions and hooks

--- src/ui/MainTable.jsx
+++ src/ui/MainTable.jsx
@@ -1,8 +1,34 @@
+import { useEffect, useState } from "react";
 import mainFiles from "../assets/mainFiles";
+import Checkbox from "./Checkbox";
 
 function MainTable() {
+  const [isCheckAll, setIsCheckAll] = useState(false);
+  const [isCheck, setIsCheck] = useState([]);
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    setList(mainFiles);
+  }, []);
+
+  const handleSelectAll = () => {
+    setIsCheckAll(!isCheckAll);
+    setIsCheck(list.map(li => li.id));
+    if (isCheckAll) {
+      setIsCheck([]);
+    }
+  };
+
+  const handleClick = e => {
+    const { id, checked } = e.target;
+    setIsCheck([...isCheck, id]);
+    if (!checked) {
+      setIsCheck(isCheck.filter(item => item !== id));
+    }
+  };
+
     return (
-      <table>
+      <table className="w-3/4">
         <thead>
           <tr>
             <th><input type="checkbox" /></th>
@@ -12,22 +38,39 @@ function MainTable() {
         </thead>
 
         <tbody>
-        {mainFiles.map((val, key) => {
+        
+          {mainFiles.map(({id, name, size}) => {
           return (
-            <tr key={key}>
-              <td>{val.checkbox}</td>
-              <td>{val.name}</td>
-              <td>{val.size}</td>
+            <tr key={id}>
+              <td>
+              <Checkbox 
+                id={id}
+                type="checkbox"
+                name={name}
+                size={size}
+                handleClick={handleClick}
+                isChecked={isCheck.includes(id)} />
+              </td>
+              <td>{name}</td>
+              <td>{size}</td>
             </tr>
           )
-        })}
-        </tbody>
-        
+          })}
+          </tbody>
 
         <tfoot>
           <tr>
-            <td><input type="checkbox" /></td>
-            <td>Select All</td>
+            <td>
+            <Checkbox
+              type="checkbox"
+              name="selectAll"
+              id="selectAll"
+              handleClick={handleSelectAll}
+              isChecked={isCheckAll}
+            />
+            </td>
+            <td>Select All
+            </td>
             <td></td>
           </tr>
         </tfoot>   
