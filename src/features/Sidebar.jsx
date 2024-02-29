cf6548b8de94c9fa28cffdeb0e2642f70dc8cd68
Bhekisile <bhekisilejozi@gmail.com>
2024-02-29 11:30:12 +0200

    Add style to header and sidebar

--- src/features/Sidebar.jsx
+++ src/features/Sidebar.jsx
@@ -3,7 +3,7 @@ import MainNav from "./MainNav";
 
 function Sidebar() {
     return (
-      <aside className="px-6 py-4 border-r-gray-200 flex flex-col gap-7 bg-slate-800 text-slate-100 border-r border-gray-50">
+      <aside className="px-6 py-4 border-r-stone-700 flex flex-col gap-7 bg-slate-800 text-slate-100 border-r border-gray-50">
         <MainNav />
         <DiskSpace />
       </aside>
